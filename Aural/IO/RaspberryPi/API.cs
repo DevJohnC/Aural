a7a9cb6b54e66370bfe035eb665d2777ccd639f4
John Carruthers <johnc@frag-labs.com>
2014-07-21 16:49:10 -0500

    Adding RaspberryPi audio output support through OpenMax APIs (HDMI and 3.5mm jack)

---
+++ Aural/IO/RaspberryPi/API.cs
@@ -0,0 +1,143 @@
+﻿// 
+// Author: John Carruthers (johnc@frag-labs.com)
+// 
+// Copyright (C) 2014 John Carruthers
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//  
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace FragLabs.Aural.IO.RaspberryPi
+{
+    /// <summary>
+    /// RaspberryPi audio API.
+    /// </summary>
+    internal class API
+    {
+        /// <summary>
+        /// Gets a value indicating if RPi audio is supported on the current platform.
+        /// </summary>
+        public static bool IsSupported { get; private set; }
+
+        static API()
+        {
+            IsSupported = false;
+            if (PlatformDetails.IsMac || PlatformDetails.IsWindows)
+                return;
+
+            try
+            {
+                var image = LibraryLoader.Load("librpiaudio.so.1");
+                if (image.Equals(IntPtr.Zero))
+                {
+                    image =
+                        LibraryLoader.Load(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Libs", "librpiaudio.so"));
+                }
+
+                if (image != IntPtr.Zero)
+                {
+                    var type = typeof (API);
+                    foreach (var member in type.GetFields(BindingFlags.Static | BindingFlags.NonPublic))
+                    {
+                        var methodName = member.Name;
+                        var fieldType = member.FieldType;
+                        if (fieldType == typeof (bool)) continue;
+                        var ptr = LibraryLoader.ResolveSymbol(image, methodName);
+                        if (ptr == IntPtr.Zero)
+                            throw new Exception(string.Format("Could not resolve symbol \"{0}\"", methodName));
+                        member.SetValue(null, Marshal.GetDelegateForFunctionPointer(ptr, fieldType));
+                    }
+                    IsSupported = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //  ignore - not a supported platform.
+                Console.WriteLine(ex);
+            }
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate IntPtr createDeviceDelegate(uint samplerate, uint channelCount, uint bitDepth, uint buffercount, uint bufferSize);
+        internal static createDeviceDelegate rpi_audio_create;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void destroyDeviceDelegate(IntPtr device);
+        internal static destroyDeviceDelegate rpi_audio_destroy;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void setDestDelegate(IntPtr device, uint dest);
+        internal static setDestDelegate rpi_audio_set_dest;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate IntPtr getBufferDelegate(IntPtr device);
+        internal static getBufferDelegate rpi_audio_get_buffer;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate IntPtr playDelegate(IntPtr device);
+        internal static playDelegate rpi_audio_play;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate IntPtr pauseDelegate(IntPtr device);
+        internal static pauseDelegate rpi_audio_pause;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate IntPtr stopDelegate(IntPtr device);
+        internal static stopDelegate rpi_audio_stop;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void queueBufferDelegate(IntPtr device, IntPtr buffer, int byteLength);
+        internal static queueBufferDelegate rpi_audio_queue_buffer;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate uint getStateDelegate(IntPtr device);
+        internal static getStateDelegate rpi_audio_get_state;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate uint getUnplayedSamplesDelegate(IntPtr device);
+        internal static getUnplayedSamplesDelegate rpi_audio_unplayed_samples;
+    }
+
+    internal enum OutputDest
+    {
+        /// <summary>
+        /// HDMI output.
+        /// </summary>
+        HDMI = 0,
+        /// <summary>
+        /// 3.5mm audio jack.
+        /// </summary>
+        AudioJack = 1
+    }
+
+    internal enum OmxState
+    {
+        Invalid = 0,
+        Loaded = 1,
+        Idle = 2,
+        Executing = 3,
+        Pause = 4,
+        WaitForResources = 5
+    }
+}
---
+++ Aural/IO/RaspberryPiOutput.cs
@@ -0,0 +1,213 @@
+﻿// 
+// Author: John Carruthers (johnc@frag-labs.com)
+// 
+// Copyright (C) 2014 John Carruthers
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//  
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//  
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using FragLabs.Aural.IO.RaspberryPi;
+
+namespace FragLabs.Aural.IO
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RaspberryPiOutput : IAudioOutput
+    {
+        private readonly OutputDest _dest;
+        /// <summary>
+        /// size of each buffer in bytes
+        /// </summary>
+        private uint _bufferSize;
+
+        private int _samplesPerBuffer;
+
+        public static RaspberryPiOutput[] GetOutputDevices()
+        {
+            if (!API.IsSupported)
+                return new RaspberryPiOutput[0];
+            return new []
+                {
+                    new RaspberryPiOutput(OutputDest.HDMI),
+                    new RaspberryPiOutput(OutputDest.AudioJack) 
+                };
+        }
+
+        private IntPtr _device;
+
+        internal RaspberryPiOutput(OutputDest dest)
+        {
+            _dest = dest;
+            Name = (dest == OutputDest.HDMI) ? "HDMI" : "3.5mm Jack";
+        }
+
+        public void Dispose()
+        {
+            if (IsOpen)
+                Close();
+        }
+
+        public bool IsPlaying
+        {
+            get
+            {
+                if (!IsOpen)
+                    return false;
+                return (OmxState) API.rpi_audio_get_state(_device) == OmxState.Executing;
+            }
+        }
+
+        public bool IsStopped
+        {
+            get
+            {
+                if (!IsOpen)
+                    return false;
+                return (OmxState)API.rpi_audio_get_state(_device) == OmxState.Idle;
+            }
+        }
+
+        public bool IsPaused
+        {
+            get
+            {
+                if (!IsOpen)
+                    return false;
+                return (OmxState)API.rpi_audio_get_state(_device) == OmxState.Pause;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating if the audio output is open.
+        /// </summary>
+        public bool IsOpen
+        {
+            get
+            {
+                return _device != IntPtr.Zero;
+            }
+        }
+
+        /// <summary>
+        /// Gets the device name.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the number of frames queued for playback. May not be precise.
+        /// </summary>
+        public int QueuedSampleCount
+        {
+            get
+            {
+                if (!IsOpen)
+                    throw new Exception("Must open device first.");
+                return (int)API.rpi_audio_unplayed_samples(_device);
+            }
+        }
+
+        /// <summary>
+        /// Gets the audio format.
+        /// </summary>
+        public AudioFormat Format { get; private set; }
+
+        public void Open(AudioFormat format)
+        {
+            if (IsOpen)
+                Close();
+            _bufferSize = (uint)(FormatHelper.SampleSize(format.BitDepth, format.Channels) * format.SampleRate) / 10;
+            _samplesPerBuffer = (int)_bufferSize/FormatHelper.SampleSize(format.BitDepth, format.Channels);
+            Format = format;
+            //  create a device that can buffer 1s of audio in 10 discrete buffers.
+            _device = API.rpi_audio_create((uint) format.SampleRate, (uint) format.Channels, (uint) format.BitDepth,
+                                           10, _bufferSize);
+            if (_device == IntPtr.Zero)
+            {
+                throw new Exception("Failed to open device.");
+            }
+            var dest = (uint) _dest;
+            API.rpi_audio_set_dest(_device, dest);
+        }
+
+        public void Close()
+        {
+            if (IsOpen)
+            {
+                API.rpi_audio_destroy(_device);
+                _device = IntPtr.Zero;
+            }
+        }
+
+        public void Play()
+        {
+            if (IsOpen)
+                API.rpi_audio_play(_device);
+        }
+
+        public void Stop()
+        {
+            if (IsOpen)
+                API.rpi_audio_stop(_device);
+        }
+
+        public void Pause()
+        {
+            if (IsOpen)
+                API.rpi_audio_pause(_device);
+        }
+
+        public unsafe int Write(byte[] pcmBuffer, int offset, int sampleCount)
+        {
+            if (!IsOpen)
+                throw new Exception("Must open device first.");
+            if (!IsPaused || !IsPlaying)
+                throw new Exception("Device must be in playing or paused state to write audio samples.");
+
+            var written = 0;
+            var sampleSize = FormatHelper.SampleSize(Format.BitDepth, Format.Channels);
+            while (written < sampleCount)
+            {
+                var buffer = API.rpi_audio_get_buffer(_device);
+                if (buffer == IntPtr.Zero) return written;
+
+                var pointer = (byte*)buffer.ToPointer();
+                var samplesToWrite = sampleCount;
+                if (samplesToWrite > _samplesPerBuffer)
+                    samplesToWrite = _samplesPerBuffer;
+
+                for (var i = 0; i < samplesToWrite; i++)
+                {
+                    for (var j = 0; j < sampleSize; j++)
+                    {
+                        pointer[(i * sampleSize) + j] = pcmBuffer[(i * sampleSize) + j + offset];
+                    }
+                }
+                API.rpi_audio_queue_buffer(_device, buffer, samplesToWrite * sampleSize);
+
+                written += samplesToWrite;
+            }
+            return written;
+        }
+    }
+
+
+}
